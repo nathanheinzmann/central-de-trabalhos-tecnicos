20752d229df2100e1368aefd616fdee9b96e92e5
Nathan Heinzmann <nathan.heinzmann@newfold.com>
2022-05-04 20:06:15 -0300

    Fix reponse types

--- src/components/Home/Response/Response.style.ts
+++ src/components/Home/Response/Response.style.ts
@@ -1,5 +1,4 @@
 import styled, { css } from 'styled-components';
-import { InfoProps } from './Response.types';
 import { DefaultContainer } from '@src/themes/common';
 
 export const Container = styled(DefaultContainer)`
